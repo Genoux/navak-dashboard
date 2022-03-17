951b290ca9cb50c2d8a7368a1c1d20dda94a4a7a
John Barry <john.olivierb@gmail.com>
2022-03-17 17:04:21 -0400

    added back ping servers as template

--- imports/api/fixtures.js
+++ imports/api/fixtures.js
@@ -1,2 +1,17 @@
 import { Meteor } from 'meteor/meteor';
-Meteor.startup(async (e) => {});
+import Servers from './collections/Servers.js';
+const isReachable = require('is-reachable');
+var cron = require('node-cron');
+Meteor.startup(async (e) => {
+  // var servers = Servers.find().fetch();
+  // cron.schedule('*/3 * * * * *', () => {
+  //   servers.forEach(async function (host) {
+  //     var status = await isReachable(host.ipAddress);
+  //     if (status) {
+  //       Servers.update({ ipAddress: host.ipAddress }, { $set: { status: status } })
+  //     } else {
+  //       Servers.update({ ipAddress: host.ipAddress }, { $set: { status: status } })
+  //     }
+  //   });
+  // });
+});
