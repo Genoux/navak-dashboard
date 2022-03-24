8fd5a2f7474ed794bfe03a1d2e05d6f21e5b9ad6
John <john.olivierb@gmail.com>
2022-03-24 02:52:01 -0400

    styling

--- imports/ui/components/Sidebar.vue
+++ imports/ui/components/Sidebar.vue
@@ -5,23 +5,23 @@
 				<div>
 					<ul>
 						<li>
-							<router-link class="relative hover:translate-x-2 hover:bg-gray-dark-light cursor-pointer text-white flex align-middle text-center my-2 p-2 rounded-lg" :to="{name: 'lanterns'}">
+							<router-link class="relative hover:translate-x-2 hover:bg-gray-dark-light cursor-pointer text-white flex align-middle text-center my-2 p-2 rounded-sm" :to="{name: 'lanterns'}">
 								<mdicon size="21" class="m-au:to" name="Lightbulb" />
 							</router-link>
 						</li>
             <li>
-              <router-link class="relative hover:translate-x-2 hover:bg-gray-dark-light cursor-pointer text-white flex align-middle text-center my-2 p-2 rounded-lg" :to="{name: 'servers'}">
+              <router-link class="relative hover:translate-x-2 hover:bg-gray-dark-light cursor-pointer text-white flex align-middle text-center my-2 p-2 rounded-sm" :to="{name: 'servers'}">
                 <mdicon size="21" class="m-auto" name="ServerSecurity" />
                 <div v-if="servers.length > 0" class="h-3 w-3 bg-status-red absolute right-0 top-0 rounded-xl animate-pulse"></div>
                  </router-link>
              	</li>
              <li>
-               <router-link class="relative hover:translate-x-2 hover:bg-gray-dark-light cursor-pointer text-white flex align-middle text-center my-2 p-2 rounded-lg" :to="{name: 'areas'}">
+               <router-link class="relative hover:translate-x-2 hover:bg-gray-dark-light cursor-pointer text-white flex align-middle text-center my-2 p-2 rounded-sm" :to="{name: 'areas'}">
                  <mdicon size="21" class="m-auto" name="GoogleMaps" />
                </router-link>
              </li>
              <li>
-               <router-link class="relative hover:translate-x-2 hover:bg-gray-dark-light cursor-pointer text-white flex align-middle text-center my-2 p-2 rounded-lg" :to="{name: 'stations'}">
+               <router-link class="relative hover:translate-x-2 hover:bg-gray-dark-light cursor-pointer text-white flex align-middle text-center my-2 p-2 rounded-sm" :to="{name: 'stations'}">
                  <mdicon size="21" class="m-auto" name="HandBackRight" />
                </router-link>
              </li>
