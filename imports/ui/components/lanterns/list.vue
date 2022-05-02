3eb948405a1f31dd427352e69ca7e663dd41e9b3
John Barry <john.olivierb@gmail.com>
2022-05-02 16:51:45 -0400

    Add number of lanterns

--- imports/ui/components/lanterns/list.vue
+++ imports/ui/components/lanterns/list.vue
@@ -3,11 +3,14 @@
 		<div class="w-full flex bg-dark border-b border-white border-opacity-20 pt-5 pb-5">
 			<h1 class="text-white ml-5 align-middle self-center content-center font-regular">
 				{{ $route.name.charAt(0).toUpperCase() + $route.name.slice(1) }}
+        <span class="opacity-50">({{ filteredList.length }})</span>
 			</h1>
+      <!-- <v-dropdown style="height: 42px;" @filterSelection="filterSelection($event)" CustomClass="w-full px-4 py-2 h-full rounded-md" :setAll="true" :selection="lanterns" default="All" filterBy="status"></v-dropdown> -->
 		</div>
+    
 		<v-serversStatus></v-serversStatus>
 		<div v-if="lanterns.length > 0" class="p-4 grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-			<div v-bind:key="object" v-for="(lantern, object) in lanterns">
+			<div v-bind:key="object" v-for="(lantern, object) in filteredList">
 				<v-lantern :lantern="lantern"></v-lantern>
 			</div>
 		</div>
@@ -24,12 +27,52 @@ import ServersStatusBanner from '../ServersStatusBanner.vue';
 export default {
 	name: 'lanterns',
 	components: {
+    'v-dropdown': () => import('../dropdown.vue'),
 		'v-lantern': singleLantern,
 		'v-serversStatus': ServersStatusBanner
 	},
 	data() {
-		return {};
+		return {
+      selected: 'All',
+    };
 	},
+  mounted () {
+  },
+  computed: {
+    filteredList() {
+      return this.computed_items.filter((post) => {
+        return post;
+      });
+    },
+    computed_items: function () {
+      if (this.selected == 'All') {
+        return this.lanterns;
+      }
+      let filterSize = this.selected;
+      return this.lanterns.filter(function (item) {
+        let filtered;
+          if (filterSize) {
+            filtered = item.status == filterSize;
+          }
+        return filtered;
+      });
+    }
+  },
+  methods: {
+    renameStatus: function () {
+      const obj = this.lanterns
+      function groupArrayOfObjects(list, key) {
+        return list.reduce(function (rv, x) {
+          (rv[x[key]] = rv[x[key]] || []).push(x);
+          return rv;
+        }, {});
+      };
+      return groupArrayOfObjects(obj, "status");
+    },
+   filterSelection(e) {
+     this.selected = e;
+   },
+  },
 	meteor: {
 		$subscribe: {
 			lanterns: []
